d625eb2fb774522414ad2758c4de5e129b8d318d
Searge <searge@ocean-labs.de>
2016-11-24 20:03:20 +0100

    Balanced evolution factor to compensate for slower progression in the game

--- EvolutionReduction_0.14.5/config.lua
+++ EvolutionReduction_0.14.5/config.lua
@@ -1,14 +1,14 @@
 -- Config file for Factorio Evolution Reduction MOD
 
 BASE_REDUCTION_FACTOR = 0.002	--same as vanilla distroy factor, the higher this number the faster the reduction per kill
-POLLUTION_FACTOR = 0.00002		--this is 133% of normal (0.000015) to compensate in some way
+POLLUTION_FACTOR = 0.00001		--this is 133% of normal (0.000015) to compensate in some way
 
 MINIMUM_EVOLUTION_FACTOR = 0.0	--the lowest point that the evolution factor can be reduced to 0.0 - 1.0
 
 --Adds a new difficulty level, still easier than normal but harder than the original Evolution Reduction difficulty
 --Set to false for original difficulty
 ALIENS_GEARING_FOR_WAR = true			--The aliens will still take blows to their evolution on loss of infrastructure but will begin to recover faster
-EVOLUTION_INCREMENT_FACTOR = 0.000002	--Base incremental factor increased pollution and time evolution factors on destruction
+EVOLUTION_INCREMENT_FACTOR = 0.000001	--Base incremental factor increased pollution and time evolution factors on destruction
 
 --	Enemy death reduction Multiplyer 
 --	the greater the number the larger the evolution reduction on death
